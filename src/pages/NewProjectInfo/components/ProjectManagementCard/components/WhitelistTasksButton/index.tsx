b4723faf3f752084351cafea9e02996f9b41e36d
nova <nova@ewell.finance>
2024-01-30 17:36:30 +0800

    chore: adjust placeholder, etc

--- src/pages/NewProjectInfo/components/ProjectManagementCard/components/WhitelistTasksButton/index.tsx
+++ src/pages/NewProjectInfo/components/ProjectManagementCard/components/WhitelistTasksButton/index.tsx
@@ -77,7 +77,7 @@ export default function WhitelistTasksButton({ whitelistId, whitelistTasksUrl, d
               Whitelist tasks are required when whitelist is enabled. Please provide a publicly accessible link that
               explains the associated tasks.
             </Text>
-            <Input placeholder="placeholder" value={urlInputValue} onChange={(e) => setUrlInputValue(e.target.value)} />
+            <Input placeholder="https://" value={urlInputValue} onChange={(e) => setUrlInputValue(e.target.value)} />
           </Flex>
           <Flex gap={16}>
             <Button className="flex-1" onClick={() => setIsWhitelistTasksModalOpen(false)}>
