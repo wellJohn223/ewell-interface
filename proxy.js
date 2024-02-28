0b24cd740f0190dd9c97b82ea1a33a6e6c0a8d52
nova <nova@ewell.finance>
2024-02-28 19:01:51 +0800

    chore: adjust dev config

--- proxy.js
+++ proxy.js
@@ -1,9 +1,6 @@
 const api = {
-  api: 'http://192.168.67.129:5011',
-  connectTokenApi: 'http://192.168.67.129:8011',
-  webLoginApi: 'http://192.168.67.127:5001',
-  webLoginConnectApi: 'http://192.168.67.127:8080',
-  webLoginGraphql: 'http://192.168.67.99:8083',
+  api: 'https://test.ewell.finance',
+  connectTokenApi: 'https://test.ewell.finance',
 };
 
 module.exports = api;
